730d3f436e47244caa97bd32e7152031cf067aa7
Tanner Marsh <tannermarsh@Tanners-MacBook-Pro.local>
2019-02-09 15:59:41 -0500

    added link functionality

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("project1", views.project1_view, name="project1"),
+    path("project2", views.project2_view, name="project2"),
+    path("project3", views.project3_view, name="project3")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "portfolio/project1.html")
+
+def project1_view(request):
+    return render(request, "portfolio/project1.html")
+
+def project2_view(request):
+    return render(request, "portfolio/project3.html")
+
+def project3_view(request):
+    return render(request, "portfolio/project3.html")
