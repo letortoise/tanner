ca9847b136d04175b8254360c4e388524b2a0e09
Tanner Marsh <tannermarsh@Tanners-MacBook-Pro.local>
2019-02-09 19:02:45 -0500

    added about page

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "portfolio/project1.html")
+    return render(request, "portfolio/about.html")
 
 def project1_view(request):
     return render(request, "portfolio/project1.html")
